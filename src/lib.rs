1a7e68fe4c4d677d1086ed276780ea9348fd376b
Richard Neumann <mail@richard-neumann.de>
2022-10-02 06:15:36 +0200

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub trait Contextmanager {
     fn exit(&mut self);
 }
 
-pub fn with<T: Contextmanager>(contextmanager: &mut T, run: impl Fn(&mut T) -> ())  {
+pub fn with<T: Contextmanager>(contextmanager: &mut T, run: impl Fn(&mut T) -> ()) {
     contextmanager.enter();
     run(contextmanager);
     contextmanager.exit();
