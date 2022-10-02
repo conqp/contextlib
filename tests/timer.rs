84e55e117e65dc26497535d82db8a32a2cdac0cb
Richard Neumann <mail@richard-neumann.de>
2022-10-02 13:45:01 +0200

    Add assertions

--- tests/timer.rs
+++ tests/timer.rs
@@ -37,7 +37,12 @@ impl Contextmanager for Timer {
 fn test_timer() {
     let mut timer = Timer::new();
     with(&mut timer, |this| {
+        assert!(this.start.is_some());
         println!("Start: {:?}, end: {:?}", this.start, this.end,);
+        assert!(this.end.is_none());
+        assert!(this.duration().is_none());
     });
+    assert!(timer.end.is_some());
+    assert!(timer.duration().is_some());
     println!("Duration: {:?}", timer.duration());
 }
